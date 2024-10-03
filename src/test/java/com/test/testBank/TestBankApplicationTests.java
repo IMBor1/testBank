fa7aa4487dc80d802418d4d83da8187fdb880ebf
IMBor1 <ilacerber1@gmail.com>
2024-10-03 14:25:05 +0300

    init

---
+++ src/test/java/com/test/testBank/TestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
